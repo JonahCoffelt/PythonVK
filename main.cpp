a4472877c9ccd0ca96c98e39ae4c63872df0db6f
Jonah Coffelt <coffelt.jonah0918@gmail.com>
2026-06-18 18:39:29 -0500

    chore: cleaned main

--- main.cpp
+++ main.cpp
@@ -24,51 +24,6 @@ static std::vector<char> readFile(const std::string& filename) {
     return buffer;
 }
 
-VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
-    // Look for RGB 32-bit pixel format and non-linear SRGB color space
-    for (const auto& availableFormat : availableFormats) {
-        if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
-            return availableFormat;
-        }
-    }
-
-    // Could rank remaining formats
-    // For now, any other format is good enough
-    return availableFormats[0];
-} 
-
-VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
-    // Look for triple buffer
-    for (const auto& availablePresentMode : availablePresentModes) {
-        if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
-            return availablePresentMode;
-        }
-    }
-
-    // Default to vsync (FIFO is guarenteed)
-    return VK_PRESENT_MODE_FIFO_KHR;
-}
-
-VkExtent2D chooseSwapExtent(GLFWwindow* window, const VkSurfaceCapabilitiesKHR& capabilities) {
-    if (capabilities.currentExtent.height != std::numeric_limits<uint32_t>::max()) {
-        return capabilities.currentExtent;
-    }
-    else {
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-
-        VkExtent2D actualExtent = {
-            static_cast<uint32_t>(width),
-            static_cast<uint32_t>(height)
-        };
-
-        actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
-        actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
-
-        return actualExtent;
-    }
-}
-
 bool isDeviceSuitable(PhysicalDevice& device) {
     if (!device.checkExtensionSupport(REQUIRED_DEVICE_EXTENSIONS)) {
         return false;
